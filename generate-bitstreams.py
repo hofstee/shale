bcafb233a1eeee0f46f6761609994794d6faba4c
Teguh Hofstee <teguhhofstee@gmail.com>
2019-08-14 19:11:12 -0700

    separate spaces into unique strings

--- generate-bitstreams.py
+++ generate-bitstreams.py
@@ -8,14 +8,14 @@ for entry in os.scandir("apps"):
             [
                 "python",
                 "garnet.py",
-                "--width 32",
-                "--height 16",
+                "--width", "32",
+                "--height", "16",
                 "--no-pd",
                 "--interconnect-only",
-                f"--input-app {cwd}/{entry.path}/bin/design_top.json",
-                f"--input-file {cwd}/{entry.path}/{entry.name}_input.raw",
-                f"--gold-file {cwd}/{entry.path}/{entry.name}_gold.raw",
-                f"--output-file {cwd}/{entry.path}/bin/{entry.name}.bs",
+                "--input-app", f"{cwd}/{entry.path}/bin/design_top.json",
+                "--input-file", f"{cwd}/{entry.path}/{entry.name}_input.raw",
+                "--gold-file", f"{cwd}/{entry.path}/{entry.name}_gold.raw",
+                "--output-file", f"{cwd}/{entry.path}/bin/{entry.name}.bs",
             ],
             cwd="deps/garnet"
         )
