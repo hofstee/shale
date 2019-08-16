1ff5267b01b21ca53802fd59cd558fb46297df33
Teguh Hofstee <teguhhofstee@gmail.com>
2019-08-15 21:51:57 -0700

    make bitstream generation report app utilization in artifact

--- generate-bitstreams.py
+++ generate-bitstreams.py
@@ -1,21 +1,38 @@
+import csv
 import os
+import re
 import subprocess
 
 cwd = os.getcwd()
-for entry in os.scandir("apps"):
-    if entry.is_dir():
-        subprocess.call(
-            [
-                "python",
-                "garnet.py",
-                "--width", "32",
-                "--height", "16",
-                "--no-pd",
-                "--interconnect-only",
-                "--input-app", f"{cwd}/{entry.path}/bin/design_top.json",
-                "--input-file", f"{cwd}/{entry.path}/{entry.name}_input.raw",
-                "--gold-file", f"{cwd}/{entry.path}/{entry.name}_gold.raw",
-                "--output-file", f"{cwd}/{entry.path}/bin/{entry.name}.bs",
-            ],
-            cwd="deps/garnet"
-        )
+cgra_utilization = re.compile(r"PE: (?P<PE>\d+) IO: (?P<IO>\d+) MEM: (?P<MEM>\d+) REG: (?P<REG>\d+)")
+
+with open(f"{cwd}/apps/utilization.csv", "w") as f:
+    w = csv.DictWriter(f, fieldnames=["name", "PE", "IO", "MEM", "REG"])
+    w.writeheader()
+
+    for entry in os.scandir("apps"):
+        if entry.is_dir():
+            p = subprocess.run(
+                [
+                    "python",
+                    "garnet.py",
+                    "--width", "32",
+                    "--height", "16",
+                    "--no-pd",
+                    "--interconnect-only",
+                    "--input-app", f"{cwd}/{entry.path}/bin/design_top.json",
+                    "--input-file", f"{cwd}/{entry.path}/{entry.name}_input.raw",
+                    "--gold-file", f"{cwd}/{entry.path}/{entry.name}_gold.raw",
+                    "--output-file", f"{cwd}/{entry.path}/bin/{entry.name}.bs",
+                ],
+                stdout=subprocess.PIPE,
+                cwd="deps/garnet",
+                text=True,
+            )
+
+            if not p.returncode:
+                util = cgra_utilization.search(p.stdout)
+                if util:
+                    d = util.groupdict()
+                    d["name"] = entry.name
+                    w.writerow(d)
