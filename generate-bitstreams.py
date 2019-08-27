405787bc821109c162c3ff2b5887fc01b52ee07f
Teguh Hofstee <teguhhofstee@gmail.com>
2019-08-27 05:14:29 -0500

    only generate bitstreams if not up to date

--- generate-bitstreams.py
+++ generate-bitstreams.py
@@ -9,13 +9,59 @@ parser = argparse.ArgumentParser()
 parser.add_argument("apps", nargs="*")
 parser.add_argument("--width", type=int, default=32)
 parser.add_argument("--height", type=int, default=16)
+parser.add_argument("--force", action="store_true")
 args = parser.parse_args()
 
 cwd = os.getcwd()
 cgra_utilization = re.compile(r"PE: (?P<PE>\d+) IO: (?P<IO>\d+) MEM: (?P<MEM>\d+) REG: (?P<REG>\d+)")
 
 def should_run_app(entry, args):
-    return entry.is_dir() and (len(args.apps) == 0 or entry.name in args.apps)
+    # Complete applications are expected to be in subdirectories of
+    # the apps folder. These subdirectories have the following
+    # information:
+    #
+    # bin/design_top.json - This is the top level CoreIR for the
+    #                       bitstream that we generate.
+    #
+    # bin/global_buffer.json - This holds the global buffer's unified
+    #                          buffer parameters we use to generate
+    #                          our access patterns.
+    #
+    # {app_name}_{data}.raw - These are binary data files containing
+    #                         the expected inputs or results for an
+    #                         application. Typically a result file
+    #                         will be called 'gold', while inputs are
+    #                         called 'inputs' or 'weights', etc.
+    #
+    # map.json - This file contains supplemental information used to
+    #            create the testbenches, such as which signals are
+    #            correlated to which names, a list of signals in the
+    #            design_top.json to trace, information about the
+    #            location of the i/o streams, and paths containing the
+    #            data to drive or expect from the global buffer.
+    if not entry.is_dir():
+        return False
+
+    # If args.apps is empty, we run all apps.
+    if not (len(args.apps) == 0 or entry.name in args.apps):
+        return False
+
+    if args.force:
+        return True
+
+    # We haven't created the bitstream yet.
+    if not os.path.exists(f"{entry.path}/bin/{entry.name}.bs"):
+        return True
+
+    # Check if the design has been modified more recently than the
+    # bitstream.
+    design_mtime = os.path.getmtime(f"{entry.path}/bin/design_top.json")
+    bitstream_mtime = os.path.getmtime(f"{entry.path}/bin/{entry.name}.bs")
+    if bitstream_mtime > design_mtime:
+        print(f"INFO: `{entry.name}` is already up to date.")
+        return False
+
+    return True
 
 with open(f"apps/utilization.csv", "w") as f:
     w = csv.DictWriter(f, fieldnames=["name", "PE", "IO", "MEM", "REG"])
@@ -38,8 +84,8 @@ with open(f"apps/utilization.csv", "w") as f:
                     "--gold-file", f"{cwd}/{entry.path}/{entry.name}_gold.raw",
                     "--output-file", f"{cwd}/{entry.path}/bin/{entry.name}.bs",
                 ],
-                stdout=subprocess.PIPE,
                 cwd="deps/garnet",
+                stdout=subprocess.PIPE,
                 text=True,
             )
 
