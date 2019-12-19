a76be9bba3fa07b29ef673b919e8480781f15137
Teguh Hofstee <5227572+hofstee@users.noreply.github.com>
2019-12-18 23:34:59 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     python_requires='>=3.7',
     install_requires = [
         "astor",
-        "cocotb @ git+http://github.com/thofstee/cocotb.git@timescale#egg=cocotb",
+        "cocotb @ git+http://github.com/cocotb/cocotb.git#egg=cocotb",
         "genesis2",
         "numpy",
         "pandas",
