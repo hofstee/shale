68d0f14c7471b6097941c461abf689f1bac07274
Teguh Hofstee <teguhhofstee@gmail.com>
2020-05-09 03:50:11 -0700

    conv_3_3 working

--- tests/test_standalone.py
+++ tests/test_standalone.py
@@ -19,6 +19,7 @@ def gb_cfg_addr(offset, controller=0):
 
 @cocotb.test()
 async def test_standalone(dut):
+    should_fail = False
     gc = AXI4LiteMaster(dut, "GC", dut.clk)
     gb = GlobalBuffer(dut, "GB", dut.clk)
 
@@ -56,7 +57,6 @@ async def test_standalone(dut):
 
     await gb.write(0, 0xdeadbeef)
     res = await gb.read(0)
-    print(res)
 
     h2h = Path("/aha/Halide-to-Hardware/apps/hardware_benchmarks")
     test = h2h / "tests/conv_3_3"
@@ -78,6 +78,8 @@ async def test_standalone(dut):
         return bs.flatten().view(np.uint64)
 
     bitstream = load_bitstream(config['bitstream'])
+
+    dut._log.info("Loading bitstream into glb")
 
     # write bitstream one at a time
     for k, data in enumerate(bitstream):
@@ -108,6 +110,8 @@ async def test_standalone(dut):
     await gc.write(gc_cfg_addr(rdl['glc.global_ier']), 0b100)
     await gc.write(gc_cfg_addr(rdl['glc.par_cfg_g2f_ier']), 0b1)
 
+    dut._log.info("Configuring tiles")
+
     # configure tiles
     await gc.write(gc_cfg_addr(rdl['glc.pc_start_pulse']), 1)  # TODO: 1 or ???
 
@@ -120,15 +124,22 @@ async def test_standalone(dut):
     # wait for interrupt to clear
     await FallingEdge(dut.GC_interrupt)
 
-    # # verify configuration is as expected
-    # addrs = bitstream.view(np.uint32)[1::2]
-    # datas = bitstream.view(np.uint32)[0::2]
-    # for addr, gold in zip(addrs, datas):
-    #     await gc.write(gc_cfg_addr('glc.cgra_config.addr'), int(addr))
-    #     await gc.write(gc_cfg_addr('glc.cgra_config.read'), 1)
-    #     data = await gc.read(gc_cfg_addr('glc.cgra_config.rd_data'))
-    #     if data != int(gold):
-    #         dut._log.error("[0x{int(addr):08x}] Expected `0x{gold:08x}` but got `0x{data:08x}`")
+    dut._log.info("Verifying configuration")
+
+    # verify configuration is as expected
+    addrs = bitstream.view(np.uint32)[1::2]
+    datas = bitstream.view(np.uint32)[0::2]
+    for addr, gold in zip(addrs, datas):
+        await gc.write(gc_cfg_addr(rdl['glc.cgra_config.addr']), int(addr))
+        await gc.write(gc_cfg_addr(rdl['glc.cgra_config.read']), 10)
+        data = await gc.read(gc_cfg_addr(rdl['glc.cgra_config.rd_data']))
+        if data != int(gold):
+            should_fail = True
+            dut._log.error(f"[0x{int(addr):08x}] Expected `0x{int(gold):08x}` but got `0x{int(data):08x}`")
+
+    if should_fail:
+        await Timer(CLK_PERIOD * 100, 'ns')
+        raise TestFailure()
 
     def load_image(filename):
         # image is stored as 8bpp raw binary data
@@ -169,9 +180,11 @@ async def test_standalone(dut):
     await gc.write(gb_cfg_addr(rdl['glb.tile_ctrl']),
                    0b01 << 6 | 0b01<<2)
 
-    await glb_cfg_st(0x10000, len(gold), controller=2)
+    await glb_cfg_st(0x80000, len(gold), controller=2)
     await gc.write(gb_cfg_addr(rdl['glb.tile_ctrl'], controller=2),
                    0b01 << 8 | 0b10<<4)
+
+    dut._log.info("Loading input into glb")
 
     # load data
     for k, data in enumerate(im.view(np.uint64)):
@@ -182,17 +195,35 @@ async def test_standalone(dut):
     await gc.write(gc_cfg_addr(rdl['glc.global_ier']), 0b001)
     await gc.write(gc_cfg_addr(rdl['glc.strm_f2g_ier']), 0b100)
 
+    dut._log.info("Running application")
+
     # execute app
     await gc.write(gb_cfg_addr(rdl['glb.tile_ctrl'], controller=0x1F),
                    0b10 << 11)
     await gc.write(gc_cfg_addr(rdl['glc.stall']), 0b00)
-    await gc.write(gc_cfg_addr(rdl['glc.soft_reset']), 10)
+    await gc.write(gc_cfg_addr(rdl['glc.soft_reset']), 17)
     await gc.write(gc_cfg_addr(rdl['glc.stream_start_pulse']), 1)
 
     # wait for completion
     await with_timeout(RisingEdge(dut.GC_interrupt), 50000, 'ns')
 
+    dut._log.info("Verifying output")
+
     # verify results
+    base = 0x80000
+    for k, data in enumerate(gold.view(np.uint64)):
+        addr = base + k * 8
+        res = await gb.read(addr)
+        # dut._log.info(f"0x{int(data):08x}")
+        # dut._log.info(res)
+        mask = 0x00FF00FF00FF00FF
+        if int(res) & mask != data:
+            should_fail = True
+            dut._log.error(f"Expected `0x{int(data):08x}` but got `0x{int(res) & mask:08x}`.")
+
+    if should_fail:
+        await Timer(CLK_PERIOD * 100, 'ns')
+        raise TestFailure()
 
     # wait a bit
     await Timer(CLK_PERIOD * 100, 'ns')
